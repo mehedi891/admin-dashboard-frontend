d8a7ee734d5a975711d3c8fc87ffbfffa5665a9c
mehedi891 <mdmehedihasan891@gmail.com>
2023-02-22 17:54:09 +0600

    front end

--- config/config.js
+++
@@ -1,9 +0,0 @@
-
-
-const dbconnect  = {
-    port: process.env.port || 5000,
-    url: 'mongodb+srv://mehedi739:OWlXV0YeplkvmWMK@cluster0.tvlze.mongodb.net/customerData'
-
-}
-
-module.exports = dbconnect;
